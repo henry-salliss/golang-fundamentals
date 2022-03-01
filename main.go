8065d4dd3c84e0c7381d8dd9de37cb6ec4f2c587
Henry Salliss <henrysalliss@gmail.com>
2022-03-01 18:08:50 +0000

    learning about types and constants

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 
-func main() {
-	name := "Henry"
-	name, age := "Henry Salliss", 21
-	fmt.Println("Hello world", name, age)
+
+func main() {  
+	//typed and untyped constants
+	var defaultName = "Sam" //allowed defaults to string untyped
+	type myString string
+	var customName myString = "Sam" //allowed typed constant is string
+	customName = defaultName //not allowed cant mix typed and untyped
+
+	// boolean constants
+	const trueConst = true
+    type myBool bool
+    var defaultBool = trueConst //allowed
+    var customBool myBool = trueConst //allowed
+    defaultBool = customBool //not allowed
+
+	// numeric constants
+	const a = 5
+    var intVar int = a
+    var int32Var int32 = a
+    var float64Var float64 = a
+    var complex64Var complex64 = a
+    fmt.Println("intVar",intVar, "\nint32Var", int32Var, "\nfloat64Var", float64Var, "\ncomplex64Var",complex64Var)
+
 }
